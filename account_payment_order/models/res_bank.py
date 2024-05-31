d64f07f6abaa0dbbdebb798741d5c7be06d45a9f
Christopher Ormaza <chris.ormaza@gmail.com>
2024-05-31 08:09:06 -0500

    FIX: remove constrain for migration process

--- account_payment_order/models/res_bank.py
+++ account_payment_order/models/res_bank.py
@@ -8,18 +8,18 @@ from odoo.exceptions import ValidationError
 class ResBank(models.Model):
     _inherit = "res.bank"
 
-    @api.constrains("bic")
-    def check_bic_length(self):
-        for bank in self:
-            if bank.bic and len(bank.bic) not in (8, 11):
-                raise ValidationError(
-                    _(
-                        "A valid BIC contains 8 or 11 characters. The BIC '%(bic)s' "
-                        "contains %(num)d characters, so it is not valid.",
-                        bic=bank.bic,
-                        num=len(bank.bic),
-                    )
-                )
+    # @api.constrains("bic")
+    # def check_bic_length(self):
+    #     for bank in self:
+    #         if bank.bic and len(bank.bic) not in (8, 11):
+    #             raise ValidationError(
+    #                 _(
+    #                     "A valid BIC contains 8 or 11 characters. The BIC '%(bic)s' "
+    #                     "contains %(num)d characters, so it is not valid.",
+    #                     bic=bank.bic,
+    #                     num=len(bank.bic),
+    #                 )
+    #             )
 
 
 # starting from v9, on res.partner.bank bank_bic is a related of bank_id.bic
